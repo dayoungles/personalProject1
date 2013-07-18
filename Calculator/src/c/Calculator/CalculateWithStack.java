e549105efcc1a3615234fc0dc16bc042f714e4d9
dayoungles <dayoungles@naver.com>
2013-07-18 12:54:55 +0900

    오류내용 주석첨부

--- Calculator/src/c/Calculator/CalculateWithStack.java
+++ Calculator/src/c/Calculator/CalculateWithStack.java
@@ -22,19 +22,19 @@ public class CalculateWithStack {
 		System.out.print("your answer is " + fStack.pop());
 		// }
 	}
-	
-	public static void scannerTest(Scanner scan){
+
+	public static void scannerTest(Scanner scan) {
 		scan.hasNext();
 	}
-	
+
 	// 쪼개서 스택에 각각 저장했다
 	private static void split2Stack(CalculateWithStack sample, Scanner scan) {
 		// System.out.println("Input calculus(?;)");
 		// String calculus = scan.nextLine();
-		
+
 		while (scan.hasNext()) {
 			char operator = ' ';
-			//int priorityNum = 0;
+			// int priorityNum = 0;
 			if (scan.hasNextFloat()) {
 				float pushFloat = scan.nextFloat();
 				// System.out.println(pushFloat);
@@ -46,10 +46,10 @@ public class CalculateWithStack {
 				stopoverCalculate(sample, priorityNum);// 우선순위 작은 연산자 들어오면 일단
 														// 계산해서 넣어줌
 			}
-			
-//			System.out.println(fStack);
-//			System.out.println(operatorStack);
-//			System.out.println(operator);
+
+			// System.out.println(fStack);
+			// System.out.println(operatorStack);
+			// System.out.println(operator);
 		}
 	}
 
@@ -58,40 +58,50 @@ public class CalculateWithStack {
 			float answer = sample.calculate(operatorStack.pop(), fStack.pop(),
 					fStack.pop());
 			fStack.push(answer);
-			//System.out.println(fStack);
-			//System.out.println(operatorStack);
+			// System.out.println(fStack);
+			// System.out.println(operatorStack);
 		}
 	}
-	
-	private static void stopoverCalculate(CalculateWithStack sample, int priorityNum){
-		while (!operatorStack.isEmpty()){
+
+	/**
+	 * if 문의 operatorStack.peek() <= priorityNum이면 답이 다르다. 왜???
+	 * 
+	 * @param sample
+	 * @param priorityNum
+	 */
+	private static void stopoverCalculate(CalculateWithStack sample,
+			int priorityNum) {
+		while (!operatorStack.isEmpty()) {
 			if (operatorStack.peek() < priorityNum)
 				break;
 			else {
-				float middleProcess = sample.calculate(operatorStack.pop(), fStack.pop(), fStack.pop());
+				float middleProcess = sample.calculate(operatorStack.pop(),
+						fStack.pop(), fStack.pop());
 				fStack.push(middleProcess);
 			}
 		}
 		operatorStack.push(priorityNum);
 	}
-	
-//	private static void stopoverCalculate(CalculateWithStack sample, int priorityNum) { // 우선순위 비교해서 계산까지.종료조건!!!!이건 비교만 하지, 쌓여있을 때는 어떻게 할지?
-//		if (operatorStack.isEmpty()) {
-//			operatorStack.push(priorityNum);
-//		} else {
-//			while(true/*isEmpty*/){//스택 픽이랑 비교해서 크거나 같을 때까지만 돈다.
-//				if (priorityNum >= operatorStack.peek()) { //우선순위가 높으면 일단 operatorStack에 쌓는다. 
-//					operatorStack.push(priorityNum);
-//					
-//				} else {
-//					float middleProcess = sample.calculate(operatorStack.pop(), fStack.pop(), fStack.pop());
-//					fStack.push(middleProcess); 
-//					operatorStack.push(priorityNum);
-//				}
-//			}
-//		}
-//	}
-	
+
+	// private static void stopoverCalculate(CalculateWithStack sample, int
+	// priorityNum) { // 우선순위 비교해서 계산까지.종료조건!!!!이건 비교만 하지, 쌓여있을 때는 어떻게 할지?
+	// if (operatorStack.isEmpty()) {
+	// operatorStack.push(priorityNum);
+	// } else {
+	// while(true/*isEmpty*/){//스택 픽이랑 비교해서 크거나 같을 때까지만 돈다.
+	// if (priorityNum >= operatorStack.peek()) { //우선순위가 높으면 일단 operatorStack에
+	// 쌓는다.
+	// operatorStack.push(priorityNum);
+	//
+	// } else {
+	// float middleProcess = sample.calculate(operatorStack.pop(), fStack.pop(),
+	// fStack.pop());
+	// fStack.push(middleProcess);
+	// operatorStack.push(priorityNum);
+	// }
+	// }
+	// }
+	// }
 
 	public int convertPriority(char operator) {
 		int a = 0;
@@ -108,13 +118,13 @@ public class CalculateWithStack {
 		case '/':
 			a = 4;
 			break;
-		case '^': 
-			a = 5; 
+		case '^':
+			a = 5;
 			break;
-		case '(': //괄호 열기 숫자 6
+		case '(': // 괄호 열기 숫자 6
 			a = 6;
 			break;
-		case ')': //괄호 닫기 숫자 0
+		case ')': // 괄호 닫기 숫자 0
 			a = 0;
 			break;
 		}
@@ -141,15 +151,11 @@ public class CalculateWithStack {
 		return 0f;
 	}
 	/*
-	public void bracketCalc(){
-		 그러면 priorityNum == 6이랑 7이 나왔을 때 예외상황을 설정해야.. 
-		 * 6이 들어오면 이 메소드로 옮겨오고,
-		 * 이 안에서 또 stopoverCalc를 이용해서 나온 값은 변수에 저장해 둔다...
-		 * 그러다가, 7이 들어오면 변수에 저장한 값을 fStack에 푸쉬..
-		 * 
-		 * if (priorityNum == 6){//6이면 7이 들어올 때까지...while?
-				float inBracketCal = sample.calculate(operatorStack.pop(), fStack.pop(), fStack.pop());
-		}
-		*/	
+	 * public void bracketCalc(){ 그러면 priorityNum == 6이랑 7이 나왔을 때 예외상황을 설정해야..
+	 * 6이 들어오면 이 메소드로 옮겨오고, 이 안에서 또 stopoverCalc를 이용해서 나온 값은 변수에 저장해 둔다... 그러다가,
+	 * 7이 들어오면 변수에 저장한 값을 fStack에 푸쉬..
+	 * 
+	 * if (priorityNum == 6){//6이면 7이 들어올 때까지...while? float inBracketCal =
+	 * sample.calculate(operatorStack.pop(), fStack.pop(), fStack.pop()); }
+	 */
 }
-
